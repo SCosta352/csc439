8b19442afe8d4e4a0ff5b6fd639d061f7d795c2c
Pawel <plinek@rocketmail.com>
2020-04-17 11:13:41 -0400

    Add files via upload
    first change

---
+++ data_analysis.sh
@@ -0,0 +1,18 @@
+# Change made by Stephanie at start of challenge 2
+#!/bin/bash #This change was made by Pawel before being pushed!
+#This will contain my new analytical method!
+#New method v2
+
+#made change #3
+
+#made change #4
+
+#python script goes here
+
+#second python script goes here
+
+Rscript myscript.R
+
+
+#This change was made by Pawel before being pushed!
+#This change was made by Stephanie after pulling!
